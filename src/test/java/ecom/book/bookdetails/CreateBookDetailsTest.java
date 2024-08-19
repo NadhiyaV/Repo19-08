80f58a95b0e5b4aa83259b6e53e3048012717d5f
NadhiyaV <qspidersnadhiya@gmail.com>
2024-08-19 19:36:04 +0530

    booklaungh

---
+++ src/test/java/ecom/book/bookdetails/CreateBookDetailsTest.java
@@ -0,0 +1,10 @@
+package ecom.book.bookdetails;
+
+import org.testng.annotations.Test;
+
+public class CreateBookDetailsTest {
+@Test
+public void createbooksdetailsPagetest() {
+	System.out.println("create books details");
+}
+}
